32075506ca80b6e2ff021adf09e5e185226d0228
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-10 21:26:46 +0600

    user and text added in commentbox

--- js/app.js
+++ js/app.js
@@ -28,7 +28,6 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  console.log(text)
   return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -53,6 +52,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post)
   const image = post.userImage;
 
   const div = document.createElement("article");
@@ -122,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
